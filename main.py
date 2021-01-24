915dceabc1941514c48bec3b9fbcb2aa0994a51a
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-01-23 18:45:45 -0700

    added database connection

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
 app = FastAPI()
 
+DATABASE_URL = os.environ['DATABASE_URL']
 
 @app.get("/")
 async def root():
