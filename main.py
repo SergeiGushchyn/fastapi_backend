81ac20a4a19e8a2677daf914d8aa4a8b34e22f71
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-01-30 22:24:27 -0700

    changed settings for json prod

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from fastapi import FastAPI
-from google.oauth2 import credentials
+from google.oauth2 import service_account
 import gspread
 import os
 import json
 
+from dotenv import dotenv_values
+
+
+if os.getenv('IS_PRODUCTION'):
+   config = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+else:
+   config = dotenv_values(".env")['GOOGLE_APPLICATION_CREDENTIALS']
+
+# from internal import config
+
 app = FastAPI()
+# settings = config.Settings()
 
 async def get_records():
-   json_str = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-   # json_data = json.loads(json_str)
-   # json_data['private_key'] = json_data['private_key'].replace('\\n', '\n')
+   json_str = config
+   json_data = json.loads(json_str)
+   json_data['private_key'] = json_data['private_key'].replace('\\n', '\n')
    scope = ['https://spreadsheets.google.com/feeds',
                'https://www.googleapis.com/auth/drive']
-   cred = credentials.Credentials.from_authorized_user_info(json_str, scope)
+   credentials = service_account.Credentials.from_service_account_info(json_data, scopes=scope)
    gc = gspread.authorize(credentials)
-   work_sheet = await gc.open("Testing API").sheet1
+   work_sheet = gc.open("Testing API").sheet1
    return work_sheet
 
 
 @app.get("/")
 async def dashboard():
    ws = await get_records()
-   return {"message": "Hello New York", "ws": ws}
+   return {"message": "Hello New York", "ws": ws.row_values(2)}
