96bd4d7e966e1021b51d0741c8ab5d4191d0d7d7
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-01-30 18:22:32 -0700

    changed settings for json

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from google.oauth2 import credentials
+import gspread
 import os
+import json
 
 app = FastAPI()
 
-DATABASE_URL = os.environ['DATABASE_URL']
+async def get_records():
+   json_str = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+   print(json_str)
+   json_data = json.loads(json_str)
+   # json_data['private_key'] = json_data['private_key'].replace('\\n', '\n')
+   scope = ['https://spreadsheets.google.com/feeds',
+               'https://www.googleapis.com/auth/drive']
+   cred = credentials.Credentials.from_authorized_user_info(json_data, scope)
+   gc = gspread.authorize(credentials)
+   work_sheet = await gc.open("Testing API").sheet1
+   return work_sheet
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def dashboard():
+   ws = await get_records()
+   return {"message": "Hello New York", "ws": ws}
