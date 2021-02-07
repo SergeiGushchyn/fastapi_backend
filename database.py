eaf8a0c8d231c07596471c0c8e39aee3f0046df9
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-02-06 19:43:48 -0700

    added database connection

--- database.py
+++ database.py
@@ -1,14 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
+from fastapi import HTTPException
+import psycopg2
+from internal.environment import dbc
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+def get_connection_and_cursor():
+   try:
+      conn = psycopg2.connect(dbc)
+      cur = conn.cursor()
+      return conn, cur
+   except:
+      raise HTTPException(status_code=500, detail="Database Error")
