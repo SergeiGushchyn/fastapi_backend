2513413350870defdddcbb7957960c6bade7ffac
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-03-04 03:03:25 -0700

    added variables

--- internal/environment.py
+++ internal/environment.py
@@ -3,10 +3,16 @@ import os
 
 creds = ''
 dbc = ''
+secret = ''
+alg = ''
 
 if os.getenv('IS_PRODUCTION'):
    creds = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
    dbc = os.getenv('DATABASE_URL')
+   secret = os.getenv('SECRET')
+   alg = os.getenv('ALGORITHM')
 else:
    creds = dotenv_values(".env")['GOOGLE_APPLICATION_CREDENTIALS']
    dbc = dotenv_values(".env")['DATABASE_URL']
+   secret = dotenv_values(".env")['SECRET']
+   alg = dotenv_values(".env")['ALGORITHM']
