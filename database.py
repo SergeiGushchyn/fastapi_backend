ebfe74d00919d3d204db68b4570366058cc39070
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-03-03 20:15:56 -0700

    addded exception

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from fastapi import HTTPException
 import psycopg2
+import psycopg2.extras
 from internal.environment import dbc
 
 def get_connection_and_cursor():
    try:
       conn = psycopg2.connect(dbc)
-      cur = conn.cursor()
+      cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
       return conn, cur
    except:
       raise HTTPException(status_code=500, detail="Database Error")
