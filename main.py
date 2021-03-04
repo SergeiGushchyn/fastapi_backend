51918437e89b355a263cf498a946d21b1778e63c
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-03-03 20:16:12 -0700

    added record router

--- main.py
+++ main.py
@@ -4,20 +4,11 @@ import gspread
 import json
 
 from routers import authentication
+from routers.api import records
 
 app = FastAPI()
 app.include_router(authentication.router)
-# async def get_records():
-#    json_str = config
-#    json_data = json.loads(json_str)
-#    json_data['private_key'] = json_data['private_key'].replace('\\n', '\n')
-#    scope = ['https://spreadsheets.google.com/feeds',
-#                'https://www.googleapis.com/auth/drive']
-#    credentials = service_account.Credentials.from_service_account_info(json_data, scopes=scope)
-#    gc = gspread.authorize(credentials)
-#    work_sheet = gc.open("Testing API").sheet1
-#    return work_sheet
-
+app.include_router(records.router)
 
 @app.get("/")
 async def root():
