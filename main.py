4ff0cb437a801a2e457bcca34d295311fa6b51f7
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-03-11 19:28:23 -0700

    addded cors middleware

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
-from google.oauth2 import service_account
-import gspread
-import json
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import authentication
 from routers.api import records
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(authentication.router)
 app.include_router(records.router)
 
