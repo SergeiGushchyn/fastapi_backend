45e96acd7d0c02ab3726b260b358d12a84b4af6a
Sergii Gushchyn <sergei.gushchyn@gmail.com>
2021-03-13 21:09:55 -0700

    added login and register models

--- models/user.py
+++ models/user.py
@@ -4,3 +4,13 @@ class User(BaseModel):
    email: str
    first_name: str
    last_name: str
+
+class Login(BaseModel):
+   email: str
+   password: str
+
+class Register(BaseModel):
+   email: str
+   password: str
+   first_name: str
+   last_name: str
